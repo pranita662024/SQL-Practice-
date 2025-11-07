0a9b8991bf59540ed1a0d05ba25c4305af8ee3a2
Pranita Shirsekar <pranitashirsekar22@gmail.com>
2025-11-07 23:26:32 +0530

    Add files via upload

---
+++ SQL WINDOW FUNCTIONS.sql
@@ -0,0 +1,18 @@
+-- WINDOW FUNCTIONS 
+
+USE parks_and_recreation;
+ 
+ 
+ SELECT dem.first_name , dem.last_name , gender, salary, AVG(salary) OVER(PARTITION BY gender ORDER BY dem.employee_id) AS Rolling_total
+ FROM employee_demographics dem
+ JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id;
+    
+
+ SELECT dem.first_name, dem.last_name, salary, 
+ ROW_NUMBER() OVER(PARTITION BY gender ORDER BY salary DESC) AS row_num,
+ RANK() OVER(PARTITION BY gender ORDER BY salary DESC) AS rank_num,
+ DENSE_RANK() OVER(PARTITION BY gender ORDER BY salary DESC ) AS dense_rank_num
+ FROM employee_demographics dem
+ JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id
